0da4de0d7afe57b3d00a56fcd1cd45b67a5b4583
Ezira Ashenafi <eazash22@gmail.com>
2024-05-16 11:17:11 +0300

    feat: recreate bug

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const fooCookie = useCookie('foo', {
+    default: () => 'FOO'
+  })
+  const barCookie = useCookie('bar', {
+    default: () => 'BAR'
+  })
+  const bazCookie = useCookie('baz', {
+    decode(value) {
+      console.log({action: 'decode', baz: value})
+      return value
+    },
+    encode(value) {
+      console.log({action: 'encode', baz: value})
+      return value
+    },
+  })
+</script>
+<template>
+  <div>
+    <h1>Coookies</h1>
+    <div>
+      <p>{{ fooCookie }}</p>
+      <p>{{ barCookie }}</p>
+      <p>{{ bazCookie }}</p>
+    </div>
+  </div>
+</template>
